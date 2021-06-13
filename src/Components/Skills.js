a88d40f52de1462352a01d28d4257de3a1d67323
manshad <manshadchangampalli010@gmail.com>
2021-06-13 23:02:58 +0530

    language skill

--- src/Components/Skills.js
+++ src/Components/Skills.js
@@ -1,9 +1,17 @@
 import React from 'react'
 
-function Skills() {
+function Skills({skill,progress}) {
     return (
         <div className="skillsection">
-            
+            <div className="skillcontainer">
+                <h5 className="skill-title">{skill}</h5>
+                <div className="skill-bar">
+                    <p className="skill-text">{progress}</p>
+                    <div className="skill-progress">
+                        
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
--- src/Pages/AboutPage.js
+++ src/Pages/AboutPage.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import Title from '../Components/Title'
 import ImageSection from '../Components/ImageSection'
+import Skills from '../Components/Skills'
 
 function AboutPage() {
     return (
         <div className="aboutpage">
            <Title title="About Me" span="About Me" />
            <ImageSection/>
+           <Title title="My Skills" span="My Skills" />
+           <Skills skill= "Javascript" progress= '70%' />
         </div>
     )
 }
